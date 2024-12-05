96f96fb4907f5b61a49dec87e6b033a7b9c92079
mg4506 <munish4506.be21@chitkara.edu.in>
2024-12-05 15:02:25 +0530

    add rail

--- Schema/Meeting.js
+++ Schema/Meeting.js
@@ -1,10 +1,20 @@
 const mongoose = require('mongoose');
+const timeRegex = /^([01]\d|2[0-3]):([0-5]\d)$/;
 
 const MeetingSchema = new mongoose.Schema({
   meetingName: String,
   meetingLocation: String,
   meetingDate: Date,
-  meetingTime: String,
+  meetingTime: { 
+    type: String, 
+    required: true, 
+    validate: {
+      validator: function (v) {
+        return timeRegex.test(v);
+      },
+      message: props => `${props.value} is not a valid time format (HH:mm)!`
+    }
+  }
 
 });
 
