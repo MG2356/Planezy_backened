a1b614b81f871938dd668d686595704bbc51380a
mg4506 <munish4506.be21@chitkara.edu.in>
2024-12-13 14:49:38 +0530

    changes in activity

--- Schema/Activity.js
+++ Schema/Activity.js
@@ -1,13 +1,34 @@
 const mongoose = require('mongoose');
+const timeRegex = /^([01]\d|2[0-3]):([0-5]\d)$/;
 
 const ActivitySchema = new mongoose.Schema({
     eventName: String,
     venue:String,
     address: String,
-    startDate: String,
-    endDate: String,
-    startTime: String,
-    endTime: String,
+    startDate: Date,
+    endDate: Date,
+    // startTime: String,
+    startTime: { 
+        type: String, 
+        required: true, 
+        validate: {
+          validator: function (v) {
+            return timeRegex.test(v);
+          },
+          message: props => `${props.value} is not a valid time format (HH:mm)!`
+        }
+      },
+      endTime: { 
+        type: String, 
+        required: true, 
+        validate: {
+          validator: function (v) {
+            return timeRegex.test(v);
+          },
+          message: props => `${props.value} is not a valid time format (HH:mm)!`
+        }
+      },
+    //endTime: String,
     description: String,
     organizerName: String,
     organizerContact: Number,
